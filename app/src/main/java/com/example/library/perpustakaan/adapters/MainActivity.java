aa618fb3a9deaed15dbd6e4fbb6433a7111c16e3
Mas 'Edi <aslekmak54@gmail.com>
2022-01-24 10:07:16 +0700

    Ini adalah pesan commit pertama anda

---
+++ app/src/main/java/com/example/library/perpustakaan/adapters/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.library.perpustakaan.adapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.widget.Button;
+
+import com.example.library.R;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        buttonLogin = findViewById(R.id.btnLogin);
+        buttonLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                Intent pindah = new Intent(MainActivity.this, login.class);
+                startActivity(pindah);
+            }
+
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/library/perpustakaan/adapters/pinjaman.java
@@ -0,0 +1,17 @@
+package com.example.library.perpustakaan.adapters;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.library.R;
+
+public class pinjaman extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+    }
+}
